47f956087aa0afb32c759e87b50b52752430a8bf
Luis Fernando Ruiz <luisfruizlopez@gmail.com>
2021-03-22 21:23:27 -0500

    SQL Script

--- ETL/ETL_CreateTables.sql
+++ ETL/ETL_CreateTables.sql
@@ -1,7 +1,8 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/IPuwB1
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
+DROP TABLE IF EXISTS info, college, teams, combine, draft;
+Drop Table If Exists player;
 
 CREATE TABLE "player" (
     "player_id" SERIAL   NOT NULL,
@@ -13,41 +14,37 @@ CREATE TABLE "player" (
 
 CREATE TABLE "info" (
     "player_id" INT   NOT NULL,
-    "position" VARCHAR   NOT NULL,
-    "height" FLOAT,
-    "weight" FLOAT,
+    "position" VARCHAR,
+    "Height" FLOAT,
+    "Weight" FLOAT,
     "year" INT   NOT NULL
 );
 
 CREATE TABLE "college" (
     "player_id" INT   NOT NULL,
     "school" VARCHAR,
-	"conference" VARCHAR,
---     "latitud" FLOAT   NOT NULL,
---     "longitud" FLOAT   NOT NULL
-);
+	"conference" VARCHAR);
 
 CREATE TABLE "teams" (
     "player_id" INT   NOT NULL,
-    "nfl_team" VARCHAR   NOT NULL
+    "nfl_team" VARCHAR,
+	"Conference" VARCHAR
 );
 
 CREATE TABLE "combine" (
     "player_id" INT   NOT NULL,
-    "40yd" FLOAT   NOT NULL,
-    "Vertical" FLOAT   NOT NULL,
-    "Bench" FLOAT   NOT NULL,
-    "Broad Jump" FLOAT   NOT NULL,
-    "3Cone" FLOAT   NOT NULL,
-    "Shuttle" FLOAT   NOT NULL
+    "Forty_Yard" FLOAT,
+    "Vertical" FLOAT,
+    "Bench" FLOAT,
+    "Broad_Jump" FLOAT,
+    "Three_Cone" FLOAT,
+    "Shuttle" FLOAT
 );
 
 CREATE TABLE "draft" (
     "player_id" INT   NOT NULL,
-    "Round" INT   NOT NULL,
-    "Pick_no" INT   NOT NULL,
-    "Conference" INT   NOT NULL
-);
+    "Round" INT,
+    "Pick_no" INT);
 
 ALTER TABLE "info" ADD CONSTRAINT "fk_info_player_id" FOREIGN KEY("player_id")
 REFERENCES "player" ("player_id");
