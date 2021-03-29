9b5363cc9daff8a2825c6e0101c4f2cd1d659c96
hjfeldy <harry.feldman99@gmail.com>
2021-03-29 11:36:21 -0500

    easier json objects for js parsing

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # 1.) Serve html and javascript files to the user. 
 # 2.) Query the remote database and deliver JSON data 
 #     for our javascript files to interact with
-
+import numpy as np
 from flask import Flask, jsonify, render_template
 from sqlalchemy import create_engine, desc, func
 from sqlalchemy.ext.automap import automap_base
@@ -89,43 +89,76 @@ def playerName(player):
 
 # Get number of drafted players for each position
 # Optionally fliter by year
-@app.route('/positionTrend/')
 @app.route('/positionTrend/<year>')
-def posTrend(year = None):
+def posTrend(year):
     sesh = Session()
     df = pd.read_sql(sesh.query(Info.position,
-                                func.count(Info.position))\
+                                func.count(Info.position),
+                                func.avg(Draft.Pick_No))\
+                                .join(Draft, Draft.player_id == Info.player_id)
                                 .group_by(Info.position)\
+                                .filter(Info.Year == year)\
                                 .statement,sesh.bind)
-    if year:
-        df = pd.read_sql(sesh.query(Info.position,
-                                    func.count(Info.position))\
-                                    .group_by(Info.position)\
-                                    .filter(Info.Year == year)\
-                                    .statement,sesh.bind)
-    df.columns = ['Position', 'Count']
+    df.columns = ['Position', 'Count', 'Avg']
     df = df[df['Count'] != 1]
     out = []
     for index, row in df.iterrows():
         cnt = row['Count']
         pos = row['Position']
-        out.append({'Count': cnt, 
-                   'Position':pos}) 
+        avg = row['Avg']
+        out.append({
+                   'Position':pos,
+                   'Count': cnt,
+                   'Avg Pick_No':avg }) 
+    sesh.close()
+    return jsonify(out)
+
+@app.route('/byYear/')
+def countByYear():
+    sesh = Session()
+    df = pd.read_sql(sesh.query(Info.player_id,
+                                Info.Year,
+                                Info.position)\
+                    .join(Draft, Info.player_id == Draft.player_id)\
+                    .join(Combine, Info.player_id == Combine.player_id)\
+                    .statement, sesh.bind)
+        
+    cols = list(df.columns)
+    newCols = []
+    for col in cols:
+        newCols.append(col.lower())
+    df.columns = newCols
+    # setup output df
+    years = list(df['year'].unique())
+    positions = list(df['position'].unique())
+    out = {}
+    for year in years:
+        try:
+            out[str(year)] = []
+        except:
+            continue
+    # get counts of positions by year and round
+    for year in out.keys():
+        for pos in positions:
+            try:
+                frame = df[(df['year'] == int(year)) & (df['position'] == pos)]
+                out[year].append({'Position': pos, 'Count': frame.shape[0]})
+            except KeyError:
+                continue
+    sesh.close()
     return jsonify(out)
 
 # Get aggregate statistics for a specific position,
 # on a specific year, 
 # for a specific set of combine events
-@app.route('/stats/<pos>/<year>/<stats>')
-def byYear(pos, year, stats):
-    statList = stats.split('&')
-    for i in range(len(statList)):
-        statList[i] = statList[i].lower()
+@app.route('/stats/')
+def byYear():
     sesh = Session()
     df = pd.read_sql(sesh.query(Info.player_id,
                                 Info.Height,
                                 Info.Weight,
                                 Info.Year,
+                                Info.position,
                                 Combine.Vertical,
                                 Combine.Forty_Yard,
                                 Combine.Bench,
@@ -136,25 +169,42 @@ def byYear(pos, year, stats):
                                 Draft.Pick_No)\
                     .join(Draft, Info.player_id == Draft.player_id)\
                     .join(Combine, Info.player_id == Combine.player_id)\
-                    .filter(Info.Year == year)\
-                    .filter(Info.position == pos)\
                     .statement, sesh.bind)
+    print(df)
     cols = list(df.columns)
     newCols = []
     for col in cols:
         newCols.append(col.lower())
     df.columns = newCols
-    print(df[statList])
-    output = {'Avg': {},
-              'Max': {},
-              'Min': {}}
+    # setup output df
+    years = list(df['year'].unique())
+    rounds = list(df['round'].unique())
+    positions = list(df['position'].unique())
+    out = {}
+    for year in years:
+        out[str(year)] = {}
+        for r in rounds:
+            try:
+                out[str(year)]['round ' + str(int(r))] = [] 
+            except:
+                continue
+    # Populate stats
+    statList = ['vertical', 'forty_yard','bench', 'broad_jump','three_cone','shuttle']
     for stat in statList:
-        output['Avg'][stat] = df[stat].mean()
-        output['Max'][stat] = df[stat].max()
-        output['Min'][stat] = df[stat].min()
+        for year in years:
+            for r in rounds:
+                for pos in positions:
+                    frame = df[(df['year'] == year) & (df['round'] == r) & (df['position'] == pos)][stat].dropna()
+                    if frame.size == 0 or r == np.nan:
+                        continue
+                    out[str(year)]['round ' + str(int(r))].append({'Position': pos,
+                                         'Statistic': stat,
+                                         'Count': frame.size,
+                                         'Avg': frame.mean(),
+                                         'Min':frame.min(),
+                                         'Max':frame.max()})
     sesh.close()
-
-    return jsonify(output)
+    return jsonify(out)
 
 #routes to seconday htmls 
 #Player position
