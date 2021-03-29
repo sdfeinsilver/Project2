5fa2139e36abc69ba9aaf202553c8eea780b2d47
Andrey Tokarev <atokarev1@gmail.com>
2021-03-28 18:37:20 -0600

    corrected HTML page Data Position Frequency and added bar grpach sceleton

--- static/js/DraftPositionFrequency.js
+++ static/js/DraftPositionFrequency.js
@@ -18,4 +18,18 @@ bar_layout = {
             tickmode: "linear"}, 
 
 };
+var years = [2016,2017,2018,2019,2020]
+years.forEach(year => {
+    var dropdown1 = d3.select("#selDataset1");
+    dropdown1.append("option").text(year)
+});
+
+var rounds = [1,2,3,4,5,6,7]
+rounds.forEach(round => {
+    var dropdown2 = d3.select("#selDataset2");
+    dropdown2.append("option").text(round)
+});
+
 Plotly.newPlot("bar", bar_data, bar_layout);
+
+
