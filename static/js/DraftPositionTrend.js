2018c748133283c2d8df2a1deb6b0f6d3275d195
hjfeldy <harry.feldman99@gmail.com>
2021-03-29 20:11:24 -0500

    please let me merge

--- static/js/DraftPositionTrend.js
+++ static/js/DraftPositionTrend.js
@@ -4,28 +4,64 @@ console.log(data);
 
 });
 
-line_data = [{
-    type: 'line',
-    x: [2016,2017,2018,2019,2020],
-    y: [22, 45, 32, 34, 23],
-    text: "test",
-    orientation: 'h'
-}];
+function genChart(position) {
+    d3.json('/byYear/').then(function(data) {
+        let years = [2016, 2017, 2018, 2019, 2020];
+        let y = []
+        years.forEach(function(year) {
+            let total = 0;
+            data[year].forEach(function(element) {
+                if(element.Position == position) {
+                    total += element.Count;
+                
+                }
+            })
+            y.push(total)
+        })
+        let line_layout = {      
+            title: "Draft Position Trend",
+            yaxis: {
+                    tickmode: "linear"}, 
+        
+        };
+        let line_data = [{
+            type: 'line',
+            x: years,
+            y: y,
+            text: "test",
+            orientation: 'h'
+        }];
 
-line_layout = {      
-    title: "Draft Position Trend",
-    yaxis: {
-            tickmode: "linear"}, 
+        Plotly.newPlot("line", line_data, line_layout);
+    })
+}
+// line_data = [{
+//     type: 'line',
+//     x: [2016,2017,2018,2019,2020],
+//     y: [22, 45, 32, 34, 23],
+//     text: "test",
+//     orientation: 'h'
+// }];
 
-};
-var positions = ["C","OG","OT","QB","RB","WR","TE"]
+// line_layout = {      
+//     title: "Draft Position Trend",
+//     yaxis: {
+//             tickmode: "linear"}, 
+// };
+
+var positions = ["TE", "K", "CB", "OT", "QB", "WR", "S", "DT", "DE", "C", "OLB", "RB", "ILB", "P", "OG", "FB", "LS", "EDGE", "LB", "OL", "DL"]
 positions.forEach(position => {
     var dropdown1 = d3.select("#selDataset1");
     dropdown1.append("option").text(position)
 });
 
+d3.select('#selDataset1').on("change", function() {
+    let position = this.value;
+    genChart(position);
+});
 
-Plotly.newPlot("line", line_data, line_layout);
+
+// Plotly.newPlot("line", line_data, line_layout);
 
 // var dropdown1.on("change", function() {
 //     var selected_data = years.filter(x => x == this.value);
